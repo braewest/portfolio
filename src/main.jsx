950ccc85a325eabd7c4a134ef0c74bfb0c1ee79e
Braeden West <braedenwest1@gmail.com>
2026-03-13 12:23:54 -0700

    Updated path

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
