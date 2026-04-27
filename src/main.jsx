97e44d13a81ebf7c7ccf4d1cf47ac09d97153b87
Braeden West <braedenwest1@gmail.com>
2026-04-26 22:09:48 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/portfolio/">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
